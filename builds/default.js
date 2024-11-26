4b24ec928b2aa6fe0cffc6dd9d6b1ea04ffcf75e
Souler <juanjoseherrero@playtomic.io>
2024-11-26 22:28:09 +0100

    feat: update config  default for sovol sv05

--- builds/default.js
+++ builds/default.js
@@ -1,13 +1,14 @@
 module.exports = {
-    board_env: "STM32F103RE_creality",
+    board_env: "mega2560",
     min_version: "2.1.2",
+    only: "stable",
     meta: {
-        stable_name: "ender_3_4.2.2-{{marlin_version}}-default-{{uid}}",
-        nightly_name: "ender_3_4.2.2-{{current_date}}-default-{{uid}}"
+        stable_name: "sovol_05_-{{marlin_version}}-default-{{uid}}",
+        nightly_name: "sovol_05_-{{current_date}}-default-{{uid}}",
     },
     based_on: {
         repo: "https://github.com/MarlinFirmware/Configurations/",
-        path: "/config/examples/Creality/Ender-3/CrealityV422/",
+        path: "/config/examples/Sovol/SV-05",
         stable_branch: "release-{{marlin_version}}",
         nightly_branch: "bugfix-2.1.x"
     }
